971a7e239865c69b595351cb11b8719f7b01fea8
Mikhail Obidin <mr.santak@gmail.com>
2024-03-23 21:11:20 +0100

    Enhance prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,5 +1,12 @@
 
 function () {
-  RPROMPT=''
-  PROMPT='%F{white}%~%f %B%F{blue}>%f%b '
+  autoload -Uz vcs_info
+  precmd_functions+=(vcs_info)
+  setopt prompt_subst
+
+  zstyle ':vcs_info:*' enable git
+  zstyle ':vcs_info:git:*' formats '%F{yellow}λ:%f%F{blue}%b%f %u%c'
+  zstyle ':vcs_info:git:*' check-for-changes true
+  RPROMPT='$vcs_info_msg_0_'
+  PROMPT='%F{white}%~%f %B%(?.%F{blue}.%F{red})>%f%b '
 }
