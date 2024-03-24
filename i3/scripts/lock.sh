b66ee925f5dbf68cc57561d1f8940c0b850229d8
Mikhail Obidin <mr.santak@gmail.com>
2024-03-24 19:01:08 +0100

    Update lock screen

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 readonly LOCK_IMAGE=/tmp/i3lock.png
 
 scrot -o "${LOCK_IMAGE}"
-convert "${LOCK_IMAGE}" -scale 10% -scale 1000% "${LOCK_IMAGE}"
+convert "${LOCK_IMAGE}" -scale 5% -scale 2000% "${LOCK_IMAGE}"
 
 i3lock -u -i "${LOCK_IMAGE}"
