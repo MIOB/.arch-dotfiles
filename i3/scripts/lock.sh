8db6f02ea319864bead3cc975e9297239adc9c60
Mikhail Obidin <mr.santak@gmail.com>
2024-09-11 21:47:00 +0200

    Show keyboard loayout on lock screen

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -7,5 +7,5 @@ readonly LOCK_IMAGE=/tmp/i3lock.png
 scrot -o "${LOCK_IMAGE}"
 convert "${LOCK_IMAGE}" -scale 5% -scale 2000% "${LOCK_IMAGE}"
 
-i3lock --image="${LOCK_IMAGE}" --ignore-empty-password --show-failed-attempts --nofork
+i3lock --image="${LOCK_IMAGE}" --ignore-empty-password --show-failed-attempts --show-keyboard-layout --nofork
 
